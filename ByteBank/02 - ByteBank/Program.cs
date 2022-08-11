51396b11ef722514cbbf175f0bf28baf21196a4d
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-08-11 12:50:27 -0300

    Projeto 02 - Valores padrões nos tipos

---
+++ ByteBank/02 - ByteBank/Program.cs
@@ -0,0 +1,9 @@
+﻿ContaCorrente conta = new ContaCorrente();
+
+conta.titular = "Gabriela";
+conta.saldo = 200;
+
+Console.WriteLine(conta.titular);
+Console.WriteLine(conta.agencia);
+Console.WriteLine(conta.saldo);
+Console.ReadKey();
