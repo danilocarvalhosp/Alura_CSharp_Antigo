4ae4902eb3248662fdfcc55ca927f1be11daca20
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-08-11 17:28:09 -0300

    Projeto 05 - composição de classes

--- ByteBank/05 - ByteBank/Program.cs
+++ ByteBank/05 - ByteBank/Program.cs
@@ -2,3 +2,19 @@
 
 Cliente gabriela = new Cliente();
 
+gabriela.nome = "Gabriela";
+gabriela.profissão = "Desenvolvedora C#";
+gabriela.cpf = "434.562.878-20";
+
+ContaCorrente conta = new ContaCorrente();
+conta.titular = gabriela;
+conta.saldo = 500;
+conta.agencia = 563;
+conta.numero = 5634527;
+
+conta.titular.nome = "Gabriela Costa";
+
+Console.WriteLine(gabriela.nome);
+Console.WriteLine(conta.titular.nome);
+
+Console.ReadKey();
