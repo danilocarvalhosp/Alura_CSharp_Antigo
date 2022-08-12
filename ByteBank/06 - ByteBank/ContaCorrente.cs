69ef5e2ef46c2b8456ef856798cb667f0840ecb2
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-08-12 12:38:29 -0300

    Projeto 06 - ObterSaldo( ) e DefinirSaldo( )

---
+++ ByteBank/06 - ByteBank/ContaCorrente.cs
@@ -0,0 +1,54 @@
+﻿
+namespace _06___ByteBank;
+public class ContaCorrente
+{
+    public Cliente? titular;
+    public int agencia;
+    public int numero;
+    private double saldo = 100;
+
+    public double ObterSaldo()
+    {
+        return saldo;
+    }
+
+    public void DefinirSaldo(double saldo)
+    {
+        if (saldo < 0)
+        {
+            return;
+        }
+        else
+        {
+            this.saldo = saldo;
+        }
+    }
+
+    public bool Sacar(double valor)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+
+        this.saldo -= valor;
+        return true;
+    }
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
+    }
+
+    public bool Transferir(double valor, ContaCorrente contaDestino)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+
+        this.saldo -= valor;
+        contaDestino.Depositar(valor);
+        return true;
+    }
+}
---
+++ ByteBank/06 - ByteBank/Program.cs
@@ -0,0 +1,10 @@
+﻿using _06___ByteBank;
+
+ContaCorrente conta = new ContaCorrente();
+
+conta.DefinirSaldo(-10);
+
+Console.WriteLine(conta.ObterSaldo());
+
+Console.ReadKey();
+
