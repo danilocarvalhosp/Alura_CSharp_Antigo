c07ea5e08e09672b5da2632853431eeddb78b0c1
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-08-15 16:11:56 -0300

    Criando a classe Funcionário

---
+++ ByteBank2/ByteBank2/Program.cs
@@ -0,0 +1,10 @@
+﻿using ByteBank2.Funcionarios;
+
+Funcionario carlos = new Funcionario(1);
+
+carlos.Nome = "Carlos";
+carlos.CPF = "546.879.157-20";
+carlos.Salario = 2000;
+
+Console.WriteLine(carlos.Nome);
+Console.WriteLine(carlos.GetBonificacao());
