61b5e780cf2b1ab40efe1c9f6211f343564125e2
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-08-15 12:20:27 -0300

    Projeto 07 - Construtores

---
+++ ByteBank/07 - ByteBank/Program.cs
@@ -0,0 +1,11 @@
+﻿using _07___ByteBank;
+
+ContaCorrente conta = new ContaCorrente(867, 86712540);
+
+// conta.Numero = 86712540;
+// conta.Agencia = 867;
+
+Console.WriteLine(conta.Agencia);
+Console.WriteLine(conta.Numero);
+
+Console.ReadKey();
