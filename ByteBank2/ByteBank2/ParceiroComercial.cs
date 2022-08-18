e1684f81d0ed42d0312a152225cb45ac8a155c36
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-08-18 17:03:58 -0300

    Problema da herança múltipla

---
+++ ByteBank2/ByteBank2/ParceiroComercial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank2
+{
+    public class ParceiroComercial
+    {
+        public string Senha { get; set; }
+        public bool Autenticar(string senha)
+        {
+            return Senha == senha;
+        }
+    }
+}
