1b95b2acb3a02c7062b6df5b43d91fdfbde3e35e
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-08-19 12:15:52 -0300

    Conhecendo exceções

--- ByteBank/02 - ByteBank/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿ContaCorrente conta = new ContaCorrente();
-
-conta.titular = "Gabriela";
-conta.saldo = 200;
-
-Console.WriteLine(conta.titular);
-Console.WriteLine(conta.agencia);
-Console.WriteLine(conta.saldo);
-Console.ReadKey();
