02529066a704ee5977dc635f83477971e2ae88c8
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-08-19 18:28:01 -0300

    Tratando várias exceções

--- ByteBank/ByteBank/Program.cs
+++ ByteBank/ByteBank/Program.cs
@@ -10,40 +10,38 @@ namespace ByteBank
             {
                 Metodo();
             }
-            catch (NullReferenceException erro2)
-            {                
-                Console.WriteLine(erro2.StackTrace);
+            catch (DivideByZeroException erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.WriteLine("Não é possível divisão por zero.");
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.WriteLine(erro.StackTrace);
                 Console.WriteLine("Aconteceu um erro!");
             }
 
             Console.ReadLine();
         }
+
         //Teste com a cadeia de chamada:
         //Metodo -> TestaDivisao -> Dividir
         private static void Metodo()
         {
-            TestaDivisao(2);
+            TestaDivisao(0);
         }
 
         private static void TestaDivisao(int divisor)
         {
-            try
-            {
-                int resultado = Dividir(10, divisor);
-                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
-            }
-            catch (DivideByZeroException erro)
-            {
-                Console.WriteLine(erro.Message);
-                Console.WriteLine(erro.StackTrace);
-                Console.WriteLine("Não é possível fazer uma divisão por zero.");
-            }
+            int resultado = Dividir(10, divisor);
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
         }
 
         private static int Dividir(int numero, int divisor)
         {
             ContaCorrente conta = null;
-            Console.WriteLine(conta.Saldo);
+            // Console.WriteLine(conta.Saldo);
 
             return numero / divisor;
         }
