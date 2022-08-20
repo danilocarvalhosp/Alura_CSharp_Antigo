462cf7dab48612fefcf70c254db5f2b6a4a0e76c
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-08-20 17:13:23 -0300

    Usando o Humanizer

--- SistemaAgencia/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ SistemaAgencia/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -1,38 +1,21 @@
 ﻿using ByteBank;
 using ByteBank.Modelos;
+using Humanizer;
 
 internal class Program
 {
     private static void Main(string [] args)
     {
-        DateTime dataFimPagamento = new DateTime(2022, 09, 21);
+        DateTime dataFimPagamento = new DateTime(2023, 08, 21);
         DateTime dataCorrente = DateTime.Now;
 
-        TimeSpan diferenca = dataFimPagamento - dataCorrente;
+        TimeSpan diferenca = TimeSpan.FromMinutes(60); //dataFimPagamento - dataCorrente;
         //Console.WriteLine(dataFimPagamento);
         ///Console.WriteLine(dataCorrente);
 
-        string mensagem = "Vencimento em " + GetIntervaloDeTempoLegivel(diferenca);
+        string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
 
         Console.WriteLine(mensagem);
-
-        static string GetIntervaloDeTempoLegivel(TimeSpan timespan)
-        {
-            if (timespan.Days > 30)
-            {
-                int qtdeMeses = timespan.Days / 30;
-                if (qtdeMeses == 1)
-                {
-                    return "1 mês";
-                }
-                return qtdeMeses + " meses";
-            }
-            else if (timespan.Days > 7){
-                int qtdeSemanas = timespan.Days / 7;
-            }  
-
-            return timespan.Days + " dias.";
-        }
     }
 }
 
